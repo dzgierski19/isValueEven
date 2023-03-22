fb6fa20bd5326e2c65cfb02b771b914e25ebd450
dzgierski19 <dzgierskigit@gmail.com>
2023-03-23 00:40:29 +0100

    validation added

--- isValueEven.js
+++ isValueEven.js
@@ -1,7 +1,31 @@
-function isEvenValue(number) {
+function isValueEven(number) {
+  isInputNumber(number);
+  isInputAnInteger(number);
+  isInputZero(number);
   if (number / 2 === 1) {
     return true;
   } else {
     return false;
   }
 }
+
+isInputNumber = (input) => {
+  if (typeof input !== "number" || isNaN(input) === true) {
+    throw new Error("You must type number");
+  }
+};
+
+isInputAnInteger = (input) => {
+  if (Number.isInteger(input) === false) {
+    throw new Error("You must not type an integer");
+  }
+};
+
+isInputZero = (input) => {
+  if (input === 0) {
+    throw new Error("You must not type 0");
+  }
+};
+
+const result1 = isValueEven(0);
+console.log(result1);
